c897960b1910d12d705ec6dc2ead0d9902cca48c
Ivan Atanasčev <ivan.ata@seznam.cz>
2024-10-08 23:17:31 +0200

    wip: haptics, orientation

--- app/(index-stack)/_layout.tsx
+++ app/(index-stack)/_layout.tsx
@@ -1,6 +1,13 @@
 import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import { DeviceMotion } from "expo-sensors";
 
 export default function Layout() {
+  const [orientation, setOrientation] = useState("Portrait");
+
+  useEffect(() => {
+  });
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Categories" }} />
